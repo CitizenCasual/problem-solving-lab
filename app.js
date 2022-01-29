db226df7a0650bf458239223f0245573a0748ba9
Caleb Vietinghoff <vietinghoffcaleb@gmail.com>
2022-01-28 20:37:09 -0500

    Solve calculate

--- app.js
+++ app.js
@@ -21,3 +21,22 @@ function reverseWordOrder(str) {
 console.log(reverseWordOrder('Ishmael Me Call'));
 
 //*----------------------------------*//
+//*----------03calculate-------------*//
+
+function calculate(num1, num2, operation) {
+  if (operation === 'add') {
+    return num1 + num2
+  } if (operation === 'sub') {
+    return num1 - num2
+  } if (operation === 'mult') {
+    return num1 * num2
+  } if (operation === 'div') {
+    return num1 / num2
+  } if (operation === 'exp') {
+    return num1 ** num2
+  }
+}
+
+console.log(calculate(4, 3, 'mult'));
+
+//*---------------------------------*//
